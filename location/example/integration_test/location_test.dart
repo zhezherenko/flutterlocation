d59f8522e8aa1cec878c516078f06cc994f16d6d
Guillaume Bernos <guillaume@bernos.dev>
2020-12-23 21:22:10 +0100

    Update dependencies

--- location/example/integration_test/location_test.dart
+++ location/example/integration_test/location_test.dart
@@ -1,3 +1,6 @@
+// TODO(Lyokone): Remove once integration is migrated to null safety.
+// @dart = 2.9
+
 import 'package:location/location.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
--- location/example/test_driver/integration_test.dart
+++ location/example/test_driver/integration_test.dart
@@ -1,3 +1,6 @@
+// TODO(Lyokone): Remove once flutter_driver is migrated to null safety.
+// @dart = 2.9
+
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() => integrationDriver();
